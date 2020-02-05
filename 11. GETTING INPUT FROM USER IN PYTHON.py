2de8b1957cf3a591c98468de415990f016dc9a0d
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-05 20:44:29 +0500

    first code

---
+++ 11. GETTING INPUT FROM USER IN PYTHON.py
@@ -0,0 +1,11 @@
+
+
+full_Name = input('Please enter your full name: ')
+age = input("Your age: ")
+qualification = input ("Qualification: ")
+print(f"Assalam O Alaikum, {full_Name} is {age} year old with {qualification} degree.")
+
+good_Name = input("Your Good Name Please: ")
+cnic = input("Enter your CNIC Number: ")
+card_Number = input("Entry Card Number Please: ")
+print(f"Thank You, {good_Name} Wellcome to our Bank.")
